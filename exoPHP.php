34a961cf52c946ca8d899316d844519048bd4149
Khazza <mahetoquet@gmail.com>
2023-04-24 14:49:18 +0200

    start exo

---
+++ exoPHP.php
@@ -0,0 +1,6 @@
+<!-- Exercices:
+    Ecrire un script PHP qui affiche tous les nombres impairs entre 0 et 150, par ordre croissant : 1 3 5 7... :-->
+
+<!--Écrire un programme qui écrit 500 fois la phrase Je dois faire des sauvegardes régulières de mes fichiers : -->
+
+<!--Ecrire un script qui affiche la table de multiplication totale de {1,...,12} par {1,...,12} dans un tableau HTML :-->
